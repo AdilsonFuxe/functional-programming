e96f053f118df1644a67f546fbdcd86420e617df
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2020-09-26 04:15:42 +0100

    censured text with reduce

--- curry-e-composition/ex5.js
+++ curry-e-composition/ex5.js
@@ -1,14 +1,12 @@
-const { __, curry, compose, split, join, map, toLower, includes } = require('ramda');
+const { __, reduce, replace } = require('ramda');
 
 const text = 'Rodrigo você é um babaca pq programa em PHP';
 const blacklist = ['bocó', 'babaca', 'php'];
 const placeHolder = '$#@#$@*';
 
-const getWords = split(' ');
-const getSentences = join(' ');
-const isCensured = compose(includes(__, blacklist), toLower);
-const hideWords = map(word => isCensured(word) ? placeHolder: word)
 
-const censured = compose(getSentences, hideWords, getWords);
+const hideWord = (text, word) => replace(new RegExp(word, 'i'), placeHolder, text);
+
+const censured = reduce(hideWord, __, blacklist);
 
 console.log(censured(text));
