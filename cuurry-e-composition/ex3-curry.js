d6178c88fe96e76955b32c152a1021f52d8a82d3
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2020-09-26 03:40:16 +0100

    censured words with curry

---
+++ cuurry-e-composition/ex3-curry.js
@@ -0,0 +1,16 @@
+const { curry } = require('ramda');
+
+const text = 'Rodrigo você é um babaca pq programa em PHP';
+const blacklist = ['bocó', 'babaca', 'php'];
+const placeHolder = '$#@#$@*';
+
+const split = curry((separator, text) => text.split(separator));
+const getWords = split(' ');
+const join = curry((separator, list) => list.join(separator));
+const getSentences = join(' ');
+const map = curry((fn, list) => list.map(fn));
+const hideWords = map(word => blacklist.includes(word.toLowerCase())? placeHolder: word)
+
+const censured = getSentences(hideWords(getWords(text)));
+
+console.log(censured);
